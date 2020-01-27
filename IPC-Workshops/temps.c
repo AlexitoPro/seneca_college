1ac6dd4b1e077efb80410b8fdc13723de61d9a09
Alex Pan Li <alexpanli5@gmail.com>
2020-01-27 14:06:42 -0500

    Add files via upload

--- IPC-Workshops/temps.c
+++ IPC-Workshops/temps.c
@@ -11,132 +11,48 @@
 
 // Put your code below:
 
-#define NUMS 3
+#define NUMS 4
 
 int main(void)
 {
-	int i, high, low, highest, lowest, highest_1, highest_2, lowest_1, lowest_2, high1 = 0, high2 = 0, high3 = 0, high4 = 0, low1 = 0, low2 = 0, low3 = 0, low4 = 0, total_high, total_low, day;
+	int i, high, low, highest = 0, lowest = 0, highest_day, lowest_day;
+	double high_average = 0, low_average = 0;
+
 	printf("---=== IPC Temperature Analyzer ===---");
-	for (i = 1; i < 5; i++)
+
+	for (i = 1; i <= NUMS; i++)
 	{
-		do
+		printf("\nEnter the high value for day %d: ", i);
+		scanf("%d", &high);
+		printf("\nEnter the low value for day %d: ", i);
+		scanf("%d", &low);
+		while (!(high > low&& high < 41 && low > -41))
 		{
+			printf("\nIncorrect values, temperatures must be in the range -40 to 40,high must be greater than low.\n");
 			printf("\nEnter the high value for day %d: ", i);
 			scanf("%d", &high);
 			printf("\nEnter the low value for day %d: ", i);
 			scanf("%d", &low);
-			while (!(high > low && high < 41 && low > -41))
-			{
-				printf("\nIncorrect values, temperatures must be in the range -40 to 40, high must be greater than low.\n");
-				printf("\nEnter the high value for day %d: ", i);
-				scanf("%d", &high);
-				printf("\nEnter the low value for day %d: ", i);
-				scanf("%d", &low);
-			}
-			switch (i)
-			{
-			case 1:
-				high1 = high;
-				low1 = low;
-				break;
-			case 2:
-				high2 = high;
-				low2 = low;
-				break;
-			case 3:
-				high3 = high;
-				low3 = low;
-				break;
-			case 4:
-				high4 = high;
-				low4 = low;
-				break;
-			}
-		} while (!(high > low && high < 41 && low > -41));
-	}
-
-	total_low = low1 + low2 + low3 + low4;
-	total_high = high1 + high2 + high3 + high4;
-
-	if (high1 >= high2) 
-	{
-		highest_1 = high1;
-	} else
+		}		
+		high_average += high;
+		low_average += low;
+		if (high > highest)
 		{
-		highest_1 = high2;
+			highest = high;
+			highest_day = i;
 		}
-	if (high3 >= high4)
-	{
-		highest_2 = high3;
-	} else
+		if (low < lowest)
 		{
-		highest_2 = high4;
+			lowest = low;
+			lowest_day = i;
 		}
-	if (highest_1 >= highest_2) 
-	{
-		highest = highest_1;
-	} else
-		{
-		highest = highest_2;
-		}
-
-	if (low1 >= low2)
-	{
-		lowest_1 = low2;
-	} else
-		{
-		lowest_1 = low1;
-		}
-	if (low3 >= low4)
-	{
-		lowest_2 = low4;
-	} else
-		{
-		lowest_2 = low3;
-		}
-	if (lowest_1 >= lowest_2)
-	{
-		lowest = lowest_2;
-	} else
-		{
-		lowest = lowest_1;
-		}
-
-	printf("The average (mean) LOW temperature was: %.2lf\n", (double)total_low / 4);
-	printf("The average (mean) HIGH temperature was: %.2lf\n", (double)total_high / 4);
-	printf("The average (mean) temperature was: %.2lf\n", ((double)total_high + (double)total_low) / 8);
-
-	if (highest == high1)
-	{
-	day = 1;
-	} else if (highest == high2)
-		{
-		day = 2;
-		} else if (highest == high3)
-			{
-			day = 3;
-			} else if (highest == high4)
-				{
-				day = 4;
-				}
-
-	printf("The highest temperature was %d on day %d\n", highest, day);
-
-	if (lowest == low1)
-	{
-	day = 1;
-	} else if (lowest == low2)
-		{
-		day = 2;
-		} else if (lowest == low3)
-			{
-			day = 3;
-			} else if (lowest == low4)
-				{
-				day = 4;
-				}
-
-	printf("The lowest temperature was %d on day %d\n", lowest, day);
+	} 
+	
+	printf("\nThe average (mean) LOW temperature was: %.2lf", low_average / NUMS);
+	printf("\nThe average (mean) HIGH temperature was: %.2lf", high_average / NUMS);
+	printf("\nThe average (mean) temperature was: %.2lf", (low_average + high_average) / (2 * NUMS));
+	printf("\nThe highest temperature was %d, on day %d", highest, highest_day);
+	printf("\nThe lowest temperature was %d, on day %d\n", lowest, lowest_day);
 
 	return 0;
 }
